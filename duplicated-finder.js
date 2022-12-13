b09f2fd0cfd5bf6a0b72f4409fe155710dd46927
audrey <mbozorgi604@gmail.com>
2022-12-13 20:53:56 +0330

    name updated

--- duplicated-finder.js
+++ duplicated-finder.js
@@ -1,8 +1,9 @@
 
+// ***** with n*2 O notation
 const tempArray = [1,2,4,54,56,3,1,21,4,12,4,5,7,23,5,6,21,3,4,5,6,8,3,2,1,21,3,4,6,6]
 let duplicates = []
 
-function duplicatedFinder() {
+function duplicatedFinderQuadratic() {
     for(let i = 0; i < tempArray.length; i++) { //this is for visiting all items in the list
         for(let j = 0; j < i;j ++){ // this for is for visiting all left side items in the list beside visiting each item and do comparison
             if(tempArray[i] == tempArray[j]) { // this will do my comparison 
@@ -13,4 +14,7 @@ function duplicatedFinder() {
     console.log('duplicated items with n*2 algorithm', duplicates);
 }
 
-duplicatedFinder()
+duplicatedFinderQuadratic()
+
+// ***** with n notation 
+
