f9297e9a9cabefaed3782b1b3c13d4b4368e269b
audrey <mbozorgi604@gmail.com>
2022-12-13 20:47:48 +0330

    add notes

--- duplicated-finder.js
+++ duplicated-finder.js
@@ -3,10 +3,10 @@ const tempArray = [1,2,4,54,56,3,1,21,4,12,4,5,7,23,5,6,21,3,4,5,6,8,3,2,1,21,3,
 let duplicates = []
 
 function duplicatedFinder() {
-    for(let i = 0; i < tempArray.length; i++) {
-        for(let j = 0; j < i;j ++){
-            if(tempArray[i] == tempArray[j]) {
-                if(!duplicates.includes(tempArray[i])) duplicates.push(tempArray[i])
+    for(let i = 0; i < tempArray.length; i++) { //this is for visiting all items in the list
+        for(let j = 0; j < i;j ++){ // this for is for visiting all left side items in the list beside visiting each item and do comparison
+            if(tempArray[i] == tempArray[j]) { // this will do my comparison 
+                if(!duplicates.includes(tempArray[i])) duplicates.push(tempArray[i]) // includes will check if this item added in the target list or not, then do add it
             } 
         }
     }
