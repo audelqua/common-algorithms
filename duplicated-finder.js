794bd6bf5ab48fa5c9cbf68b3fc7f5cac76b0419
audrey <mbozorgi604@gmail.com>
2022-12-14 20:27:47 +0330

    notes issues fixed

--- duplicated-finder.js
+++ duplicated-finder.js
@@ -1,11 +1,11 @@
 
-// ***** with n*2 O notation
+// ***** with n^2 O notation
 const tempArray = [1,2,4,54,56,3,1,21,4,12,4,5,7,23,5,6,21,3,4,5,6,8,3,2,1,21,3,4,6,6]
 let duplicates = []
 
 function duplicatedFinderQuadratic() {
     for(let i = 0; i < tempArray.length; i++) { //this is for visiting all items in the list
-        for(let j = 0; j < i;j ++){ // this for is for visiting all left side items in the list beside visiting each item and do comparison
+        for(let j = 0; j < i; j++){ // this for is for visiting all left side items in the list beside visiting each item and do comparison
             if(tempArray[i] == tempArray[j]) { // this will do my comparison 
                 if(!duplicates.includes(tempArray[i])) duplicates.push(tempArray[i]) // includes will check if this item added in the target list or not, then do add it
             } 
@@ -16,7 +16,7 @@ function duplicatedFinderQuadratic() {
 
 duplicatedFinderQuadratic()
 
-// ***** with n notation 
+// ***** with n^1 notation 
 let myItems = [] // this is for items not duplicated in the given array
 let myDuplicatedItems = [] // this is for items been duplicated in the given array
 function duplicatedFinderLinear() {
