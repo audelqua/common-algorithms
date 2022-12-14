802c78762c16c7b11fc10c303137bc5a96614f0d
audrey <mbozorgi604@gmail.com>
2022-12-14 20:53:31 +0330

    n^2 algorithm alos done with python for duplicated finder

--- duplicated-finder.py
+++ duplicated-finder.py
@@ -1,9 +1,21 @@
 
 
-listWithDuplicatedItems = [1,2,3,5,2,8,2,4,3,1]
+listWithDuplicatedItems = [1,2,3,5,2,1,8,2,4,3,1]
+
+# duplicated finder with n^2 O notation algorithm 
+dupElements = []
+def duplicatorFinderBadWay():
+    for i in range(len(listWithDuplicatedItems)): 
+        for j in range(i):
+            if listWithDuplicatedItems[i] == listWithDuplicatedItems[j]:
+                if listWithDuplicatedItems[i] not in dupElements:
+                    dupElements.append(listWithDuplicatedItems[i])
+
+            
+duplicatorFinderBadWay()
+print(dupElements)
 
 # duplicated finder with n^1 O notation algorithm 
-
 singleElements = []
 duplicatedsElements = []
 def duplicatorFinder():
