9d3e4979879db906f34ad27f9ea8c4cb5cde931c
audrey <mbozorgi604@gmail.com>
2022-12-14 20:36:42 +0330

    duplicator n^1 finder with python done

--- duplicated-finder.py
+++ duplicated-finder.py
@@ -1,9 +1,18 @@
 
 
-listWithDuplicatedItems = [1,2,3, 5, 2, 8]
+listWithDuplicatedItems = [1,2,3,5,2,8,2,4,3,1]
 
-def duplicatedFinder():
+# duplicated finder with n^1 O notation algorithm 
+
+singleElements = []
+duplicatedsElements = []
+def duplicatorFinder():
     for index, item in enumerate(listWithDuplicatedItems):
+        if item not in singleElements: 
+            singleElements.append(item)
+        else:
+            if item not in duplicatedsElements:
+                duplicatedsElements.append(item)
+duplicatorFinder()
 
-duplicatedFinder()
-
+print(duplicatedsElements)
