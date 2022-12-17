255507ad2033ff404cb4c820ffb2e0e6619a2d0e
audrey <mbozorgi604@gmail.com>
2022-12-17 23:16:14 +0330

    add notes and comments

--- find-most-repeater-character.js
+++ find-most-repeater-character.js
@@ -3,13 +3,13 @@
 const tempStr = 'wefferferferfeferferferferferfeferferferfqqqqasasasasasasasasasasasasasasasaslalalaalalalalalallalalalala'
 
 const finder = () => {
-    const charactersPair = {}
-    let mostRepeatedTimes = 0
-    let mostRepeatedCharacter = undefined
+    const charactersPair = {} // this is for characters and the time they are repeater 
+    let mostRepeatedTimes = 0 // this is for most repeated one (number)
+    let mostRepeatedCharacter = undefined // this is for most repeated one (character)
 
-    for(let i = 0; i < tempStr.length; i++) {
-        charactersPair[tempStr[i]] = charactersPair[tempStr[i]] ? charactersPair[tempStr[i]] + 1 : 1
-        if(charactersPair[tempStr[i]] > mostRepeatedTimes) mostRepeatedTimes = charactersPair[tempStr[i]]; mostRepeatedCharacter = tempStr[i]
+    for(let i = 0; i < tempStr.length; i++) { //lets iterate on all element in the given string 
+        charactersPair[tempStr[i]] = charactersPair[tempStr[i]] ? charactersPair[tempStr[i]] + 1 : 1 // here we are adding new character to our object the key is the character and the default value is one. if the key has been exist before it means that ist repeated so the number 1 will add by 1 and this will continue till the last element
+        if(charactersPair[tempStr[i]] > mostRepeatedTimes) mostRepeatedTimes = charactersPair[tempStr[i]]; mostRepeatedCharacter = tempStr[i] // this is for saving the most repeater character and its number
     }
     console.log('mostRepeatedTimes', mostRepeatedCharacter, mostRepeatedTimes);
 }
