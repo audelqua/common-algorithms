573f51ea1942a0a223aea3184f4c4f12fec199f9
audrey <mbozorgi604@gmail.com>
2023-01-09 13:19:28 +0330

    comment and docs added into target algorithm

--- find-first-last-repeated.js
+++ find-first-last-repeated.js
@@ -2,29 +2,29 @@ const arrayOfNumbers = [1,2,3,4,5,5,5,5,5,6,7,8,9]
 
 
 function firstLastRepeatedFinder() {
-    const targetItems = {}
-    let found = false
+    const targetItems = {} // this is for all same items and their indexes
+    let found = false // this is for checking start and end point.
 
-    for(let i = 0; i < arrayOfNumbers.length; i++) {
-        let item = arrayOfNumbers[i]
-        let nextItem = arrayOfNumbers[i + 1]
+    for(let i = 0; i < arrayOfNumbers.length; i++) { // start iterating on all items in given array
+        let item = arrayOfNumbers[i] // our item
+        let nextItem = arrayOfNumbers[i + 1] // our next item for comparison 
 
-        if(item === nextItem) { // last item here
+        if(item === nextItem) { // first item found!
             found = true
             targetItems[i] = item
         }else {
-            if(found) {
+            if(found) { // check if first item found before, means now we are at the end of the repeated items. so we will also save this item too. 
                 targetItems[i] = item
                 found = false
             }
         }
     }
 
-    return {
+    return { // return stuff 
         firstItem: Object.entries(targetItems)[0],
         lastItem: Object.entries(targetItems)[Object.entries(targetItems).length - 1]
     }
 }
 
-let myItems = firstLastRepeatedFinder()
+let myItems = firstLastRepeatedFinder() // showing stuff 
 console.log(myItems);
