a34894113545d5728a7093fe0ebd37a2acdf5786
audrey <mbozorgi604@gmail.com>
2023-01-27 16:29:29 +0330

    add coment on custom map

--- map-proto.js
+++ map-proto.js
@@ -1,22 +1,23 @@
-const simpleArray = [1,2,3,4,5]
+const simpleArray = [1,2,3,4,5] // **** any given array
 
-const doubledArray = simpleArray.map(function(item) {
+const doubledArray = simpleArray.map(function(item) { // **** ## this is what we have, but how it works? $$
     return item * 2
 })
 
-Array.prototype.customMap = function(customFunction) {
+// **** Array.prototype is a way which we determine new methods
+Array.prototype.customMap = function(customFunction) { // **** its function that gives a function (the call back one on the top with ## ##) and run it for each item
     let tempArray = []
 
-    this.forEach(function(item){
-        const manItem = customFunction(item)
-        tempArray.push(manItem)
+    this.forEach(function(item){ // **** using an iteration on given array which is in function execution context or "this
+        const manItem = customFunction(item) // **** each manipulated item
+        tempArray.push(manItem) // **** push items into tempArray
     })
     
-    return tempArray
+    return tempArray // **** return temp array 
 }
 
-const someMap = simpleArray.customMap((item => {
+const someMap = simpleArray.customMap((item => { //**** this is our customFunction and when we use ___ givenArray.customMethod ___. this of method is the given array  */
     return item + 2
 }))
 
-console.log('someMap', someMap);
+console.log('someMap', someMap) // someMap [ 3, 4, 5, 6, 7 ]
