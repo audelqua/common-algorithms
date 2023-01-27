17f6196c5ac1b4c0bae8bcfc01f74530defb4eb5
audrey <mbozorgi604@gmail.com>
2023-01-27 16:53:32 +0330

    create custom reduce method

--- reduce-proto.js
+++ reduce-proto.js
@@ -4,7 +4,7 @@ const sumAllElements = customNumericElements.reduce(function(accumulator, curren
     return accumulator + currentValue
 }, initialValue)
 
-console.log('sumAllElements', sumAllElements); // sumAllElements 50
+// console.log('sumAllElements', sumAllElements); // sumAllElements 50
 
 const customNumericElements2 = [10, 10, 10]
 const initialValue2 = 20
@@ -12,10 +12,32 @@ const multiplyElements = customNumericElements2.reduce(function(accumulator, cur
     return accumulator * currentValue
 }, initialValue2)
 
-console.log('multiplyElements', multiplyElements); // multiplyElements 50
+// console.log('multiplyElements', multiplyElements); // multiplyElements 50
 
 // **** reduce method gets an array of elements, it has one variable internally. which the value is come from the custom function you defined.
 // it iterates on every elements and do this calculation. and finally return the last amount of its internal variable  
 // in first calculation there is no internal variable so it start from index 1 but you can pass initial value to it. 
+
+const someElements  = [10, 10, 10]
+const inValue = 10
+Array.prototype.customReduce = function(calculatorFunction, initialValue) {
+    let internalCalculatedAmount = undefined
+
+    if(initialValue) internalCalculatedAmount = initialValue
+    else internalCalculatedAmount = 0
+
+    this.forEach(function(item){
+        internalCalculatedAmount = calculatorFunction(internalCalculatedAmount, item)
+    })
+
+    return internalCalculatedAmount
+
+}
+
+const someVariable = someElements.customReduce(function(customAccumulator, currentValue) {
+    return customAccumulator + currentValue
+}, inValue)
+
+console.log('someVariable', someVariable);
 
 
