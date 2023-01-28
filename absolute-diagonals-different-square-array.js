97dabcfd04dd5e96ed03e9e4e2e5addf1a95a081
audrey <mbozorgi604@gmail.com>
2023-01-28 16:10:21 +0330

    square array diagonals difference finder

---
+++ absolute-diagonals-different-square-array.js
@@ -0,0 +1,13 @@
+//Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+// 1 2 3 
+// 4 5 6
+// 7 8 9
+
+const absoluteDifferenceFinder = (givenArray) => {
+    let ltr = 0
+    let rtl = 0
+    
+    for(let x = 0; x < givenArray.length; x++) {
+        
+    }
+}
