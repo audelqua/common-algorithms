50ab3f3c604306cc105d2d415df852e257394484
audrey <mbozorgi604@gmail.com>
2023-01-30 15:50:20 +0330

    add custom forEach

--- forEach-proto.js
+++ forEach-proto.js
@@ -5,3 +5,13 @@ someArray.forEach(function(item) {
 })
 
 
+const someAnotherArray = [4,1,2,3,4,5,6,76,7,8,] 
+Array.prototype.customForEach = function(someCustomCallbackFunction) {
+    for(let i = 0; i < this.length; i ++) {
+        someCustomCallbackFunction(this[i])
+    }
+}
+
+someAnotherArray.customForEach(function(item) {
+    console.log('item', item)
+})
