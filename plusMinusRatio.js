7b58874d699713e6310d7db4b6d2e6ae57106c0c
audrey <mbozorgi604@gmail.com>
2023-01-30 14:58:18 +0330

    plus minus algo added

---
+++ plusMinusRatio.js
@@ -0,0 +1,3 @@
+// you have an array of numbers, you should find ratio of pluses, minuses and zeros with max of 4 decimal in fraction.
+//arr = [1,1,0,-1,-1] => plus: 2/5 and minuses: 2/5 and zeros: 1/5
+
