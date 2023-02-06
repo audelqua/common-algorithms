a183a25bc63fa7748d97d23a1530a2f1bd63d082
audrey <mbozorgi604@gmail.com>
2023-02-06 20:52:22 +0330

    add shorter syntax

--- find-proto.js
+++ find-proto.js
@@ -5,3 +5,6 @@ const itemNumber6 = someArray.find(function(item){
     return targetItem
 })
 console.log('itemNumber6', itemNumber6);
+
+const itemNumber5 = someArray.find(item => item === 5)
+console.log('itemNumber5', itemNumber5);
