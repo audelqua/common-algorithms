507f7b811bf369e168429a99c6f17ef2ad2f22e4
audrey <mbozorgi604@gmail.com>
2023-02-06 21:14:37 +0330

    add comment on out proto

--- find-proto.js
+++ find-proto.js
@@ -11,14 +11,14 @@ console.log('itemNumber5', itemNumber5);
 
 Array.prototype.customFindMethod = function(customFunction) {
     let targetItem = undefined
-    this.forEach(function(item){
-        let currentItem = customFunction(item)
-        if(currentItem) targetItem = currentItem
-    })
-    return targetItem
+    this.forEach(function(item){ // ***** generating loop in proto to iterate on all items in array.
+        let currentItem = customFunction(item) // ***** put each item in local variable
+        if(currentItem) targetItem = currentItem // ***** check if out condition matched set out outer variable as it.
+    }) 
+    return targetItem // ***** return the found item
 }
 
-const itemNumber4 = someArray.customFindMethod(function(item) {
-    if(item === 4) return item
+const itemNumber4 = someArray.customFindMethod(function(item) { // ***** this is a callback function we pass to our proto do return calculated item in loop inside proto.
+    if(item === 4) return item // ***** implement out condition to find out item
 })
 console.log('itemNumber4', itemNumber4);
