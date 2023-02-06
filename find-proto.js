1df50f87f593bf99a4edde6411031c246152f02a
audrey <mbozorgi604@gmail.com>
2023-02-06 21:11:14 +0330

    implement find prototype

--- find-proto.js
+++ find-proto.js
@@ -8,3 +8,17 @@ console.log('itemNumber6', itemNumber6);
 
 const itemNumber5 = someArray.find(item => item === 5)
 console.log('itemNumber5', itemNumber5);
+
+Array.prototype.customFindMethod = function(customFunction) {
+    let targetItem = undefined
+    this.forEach(function(item){
+        let currentItem = customFunction(item)
+        if(currentItem) targetItem = currentItem
+    })
+    return targetItem
+}
+
+const itemNumber4 = someArray.customFindMethod(function(item) {
+    if(item === 4) return item
+})
+console.log('itemNumber4', itemNumber4);
