914e338b16dbd09ee89d2e1fbdb4ea5e93f8bd0e
audrey <mbozorgi604@gmail.com>
2023-02-16 19:19:42 +0330

    makes it extreme

--- logaritmic-time-funciton.js
+++ logaritmic-time-funciton.js
@@ -1,7 +1,7 @@
 
 
 function logarithmicTimeFunction() {
-    for(let i = 0; i < 10000000000000000000000000000000000000;  i = i + i + 1){
+    for(let i = 0; i < 10000000000000000000000000000000000000;  i = i + i + i + i + i + i +1){ // here we said that take look at i and add it to itself + 1.
         console.log('i', i);
     }
 }
