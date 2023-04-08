df2f10f605714de0197713a322f43df8bc43b941
audrey <mbozorgi604@gmail.com>
2023-04-08 19:13:57 +0330

    go for it later

--- sudoku.js
+++ sudoku.js
@@ -15,8 +15,30 @@ const exampleOne = [
     [1,2,3,4,5,6,7,8,9],
 ]
 
-const sudokuConditionChecker = (exampleOne) => {
-    console.log('exampleOne', exampleOne);
+const exampleTwo = [
+    [8,3,5,4,1,6,9,2,7],
+    [2,9,6,8,5,7,4,3,1],
+    [4,1,7,2,9,3,6,5,8],
+    [5,6,9,1,3,4,7,8,2],
+    [1,2,3,6,7,8,5,4,9],
+    [7,4,8,5,2,9,1,6,3],
+    [6,5,2,7,8,1,3,9,4],
+    [9,8,1,3,4,5,2,7,6],
+    [3,7,4,9,6,2,8,1,5]
+]
+
+const sudokuConditionChecker = (exampleTwo) => {
+    let eachColumn = {}
+    exampleTwo.forEach((col, index) => {
+        console.log(col[0]);
+
+        for(let x = 0; x < col.length; x ++) {
+           
+        }
+        // eachColumn[index] = col[index]
+        // console.log('index', index, 'col', col);
+    })
+    console.log('eachColumn', eachColumn);
 }
 
-sudokuConditionChecker(exampleOne)
+sudokuConditionChecker(exampleTwo)
